ce8087368a45c12f1fbd97136c24b8468e411bfa
Jose Garcia <rocketrito1@gmail.com>
2021-12-14 13:01:36 -0800

    commit tree b2df673beec4bc724007ba3da453b19933173ca2

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400)
+  return "This one is on me!";
+
+  //what about a value between 400 and 2000
+
+  else if (someValue > 2000 && someValue <= 2500)
+  return 'I will gladly take your thirty bucks.';
+
+  //not at all how i assumed the && operator worked
+
+  else if (someValue > 2500)
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
